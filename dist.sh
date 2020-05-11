efe6c1b56971877918cce158c2d0ddc1742d1e16
Ceyhun Kerti <ceyhun.kerti@bluecolor.io>
2020-05-11 18:49:40 +0300

    dist

---
+++ dist.sh
@@ -0,0 +1,13 @@
+sbt assembly
+rm -fr dist/*
+cp target/scala-2.13/lauda-assembly-1.0.jar dist/lauda.jar
+cp template/config.yml dist/config.yml
+cp template/logback.xml dist/logback.xml
+cp -R template/lib dist/lib
+cd dist
+tar -cvzf lauda-v1.0.tar.gz lauda.jar config.yml logback.xml lib
+rm -f *.yml
+rm -f *.jar
+rm -f *.xml
+rm -fr lib
+cd ..
